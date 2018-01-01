c8dca59f9e6209d0aeaede18563876d7fd90f7f5
Charles H. Cheng <chchench@yahoo.com>
2018-01-01 01:27:32 -0800

    Initial Commit

---
+++ LearningFun/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LearningFun
+//
+//  Created by Charles H. Cheng on 1/1/18.
+//  Copyright © 2018 chchench. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
